c43a11c0f380ff187c01836afdf149279ce3d08c
PeakDevCol <peakdevcol@gmail.com>
2024-07-22 22:45:23 -0500

    Upgrade gradle and add basics libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.hilt)
+    kotlin("kapt")
 }
 
 android {
@@ -42,4 +44,33 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //Dagger
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    //Splash
+    implementation (libs.androidx.core.splashscreen)
+    // Fragment
+    implementation(libs.androidx.fragment.ktx)
+    // Activity
+    implementation(libs.androidx.activity.ktx)
+    // ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    // LiveData
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    //Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    //Gson
+    implementation (libs.gson)
+
+
+    //lottie
+    implementation(libs.lottie)
 }
