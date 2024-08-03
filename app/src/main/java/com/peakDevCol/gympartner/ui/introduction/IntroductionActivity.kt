dd2c2b1e38f0b04ea44db76dfa5731aa3774b2b6
JuanGarcia <juanfe1299@gmail.com>
2024-08-02 20:59:44 -0500

    Add Introduction Screen

---
+++ app/src/main/java/com/peakDevCol/gympartner/ui/introduction/IntroductionActivity.kt
@@ -0,0 +1,76 @@
+package com.peakDevCol.gympartner.ui.introduction
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.peakDevCol.gympartner.databinding.ActivityIntroductionBinding
+import com.peakDevCol.gympartner.ui.login.LoginActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class IntroductionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroductionBinding
+
+    private val introductionViewModel: IntroductionViewModel by viewModels()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityIntroductionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        initUi()
+    }
+
+    private fun initUi() {
+        initListeners()
+        initObservers()
+    }
+
+    private fun initListeners() {
+        binding.loginBtn.setOnClickListener {
+            introductionViewModel.onLoginSelected()
+        }
+
+        binding.signInBtn.setOnClickListener {
+            introductionViewModel.onSingInSelected()
+        }
+
+    }
+
+    private fun initObservers() {
+        introductionViewModel.navigateToLogin.observe(this) { event ->
+            event.getContentIfNotHandled()?.let {
+                goToLogin()
+            }
+
+        }
+
+        introductionViewModel.navigateToSignIn.observe(this) {
+            it.getContentIfNotHandled()?.let {
+                goToSignIn()
+            }
+        }
+
+    }
+
+    private fun goToSignIn() {
+        startActivity(LoginActivity.create(this, "signIn"))
+
+    }
+
+    private fun goToLogin() {
+        startActivity(LoginActivity.create(this, "Login"))
+
+    }
+
+}
---
+++ app/src/main/java/com/peakDevCol/gympartner/ui/introduction/IntroductionViewModel.kt
@@ -0,0 +1,33 @@
+package com.peakDevCol.gympartner.ui.introduction
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.peakDevCol.gympartner.core.Event
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class IntroductionViewModel @Inject constructor() : ViewModel() {
+
+    private val _navigateToLogin = MutableLiveData<Event<Boolean>>()
+    val navigateToLogin: LiveData<Event<Boolean>>
+        get() = _navigateToLogin
+
+    private val _navigateToSignIn = MutableLiveData<Event<Boolean>>()
+    val navigateToSignIn: LiveData<Event<Boolean>>
+        get() = _navigateToSignIn
+
+
+
+    fun onLoginSelected() {
+        _navigateToLogin.value = Event(content = true)
+    }
+
+
+    fun onSingInSelected() {
+        _navigateToSignIn.value = Event(content = true)
+    }
+
+
+}
