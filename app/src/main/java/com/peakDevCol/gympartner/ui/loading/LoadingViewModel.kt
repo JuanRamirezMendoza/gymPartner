6e2952545dfc3658d2c93a9339c6e4428674d00b
JuanGarcia <juanfe1299@gmail.com>
2024-08-02 20:56:20 -0500

    add loading screen

---
+++ app/src/main/java/com/peakDevCol/gympartner/ui/loading/LoadingViewModel.kt
@@ -0,0 +1,8 @@
+package com.peakDevCol.gympartner.ui.loading
+
+import com.peakDevCol.gympartner.ui.login.LoginViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoadingViewModel @Inject constructor() : LoginViewModel()
