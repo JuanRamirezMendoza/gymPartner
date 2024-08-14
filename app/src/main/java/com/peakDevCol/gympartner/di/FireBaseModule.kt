1096b22e807108920a75718b969e3cc70e6943c2
JuanGarcia <juanfe1299@gmail.com>
2024-08-14 17:35:58 -0500

    provide google

--- app/src/main/java/com/peakDevCol/gympartner/di/FireBaseModule.kt
+++ app/src/main/java/com/peakDevCol/gympartner/di/FireBaseModule.kt
@@ -1,11 +1,17 @@
 package com.peakDevCol.gympartner.di
 
+import android.content.Context
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.peakDevCol.gympartner.R
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -16,11 +22,30 @@ object FireBaseModule {
 
     @Singleton
     @Provides
-    fun providerFireBaseAuth() = FirebaseAuth.getInstance()
+    fun provideFirebaseAuth() = FirebaseAuth.getInstance()
 
     @Singleton
     @Provides
     fun providerFireBaseFireStore() = Firebase.firestore
+
+    @Provides
+    @Singleton
+    fun provideGoogleSignInOptions(@ApplicationContext context: Context): GoogleSignInOptions {
+        return GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(context.getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+    }
+    
+
+    @Provides
+    @Singleton
+    fun provideGoogleSignInClient(
+        @ApplicationContext context: Context,
+        gso: GoogleSignInOptions
+    ): GoogleSignInClient {
+        return GoogleSignIn.getClient(context, gso)
+    }
 
 
 }
