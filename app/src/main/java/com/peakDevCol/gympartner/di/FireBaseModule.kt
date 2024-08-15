6686e4997ba04931ceab11f1481fe5c60bc4bc54
JuanGarcia <juanfe1299@gmail.com>
2024-08-15 12:07:55 -0500

    refactor FireBaseModule

--- app/src/main/java/com/peakDevCol/gympartner/di/FireBaseModule.kt
+++ app/src/main/java/com/peakDevCol/gympartner/di/FireBaseModule.kt
@@ -1,9 +1,10 @@
 package com.peakDevCol.gympartner.di
 
 import android.content.Context
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import android.os.Build
+import android.util.Base64
+import androidx.credentials.CredentialManager
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -13,6 +14,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import java.security.SecureRandom
 import javax.inject.Singleton
 
 
@@ -30,21 +32,33 @@ object FireBaseModule {
 
     @Provides
     @Singleton
-    fun provideGoogleSignInOptions(@ApplicationContext context: Context): GoogleSignInOptions {
-        return GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(context.getString(R.string.default_web_client_id))
-            .requestEmail()
+    fun provideGoogleIdOption(@ApplicationContext context: Context): GetGoogleIdOption {
+        return GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(false)
+            .setServerClientId(context.getString(R.string.web_client_id))
+            .setAutoSelectEnabled(true)
+            .setNonce(generateNonce())
             .build()
+
     }
-    
 
     @Provides
     @Singleton
-    fun provideGoogleSignInClient(
-        @ApplicationContext context: Context,
-        gso: GoogleSignInOptions
-    ): GoogleSignInClient {
-        return GoogleSignIn.getClient(context, gso)
+    fun provideCredentialManager(@ApplicationContext context: Context) =
+        CredentialManager.create(context)
+
+
+    private fun generateNonce(): String {
+        val nonceBytes = ByteArray(16)
+        SecureRandom().nextBytes(nonceBytes)
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            java.util.Base64.getUrlEncoder().encodeToString(nonceBytes)
+        } else {
+            Base64.encodeToString(
+                nonceBytes,
+                Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP
+            )
+        }
     }
 
 
