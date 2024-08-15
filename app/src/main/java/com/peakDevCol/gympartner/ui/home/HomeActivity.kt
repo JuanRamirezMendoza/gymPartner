73b5fa26ecee9d7c5a0770174b0d30c0f7a2ae8a
JuanGarcia <juanfe1299@gmail.com>
2024-08-15 12:06:46 -0500

    add empty home activity

---
+++ app/src/main/java/com/peakDevCol/gympartner/ui/home/HomeActivity.kt
@@ -0,0 +1,29 @@
+package com.peakDevCol.gympartner.ui.home
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.peakDevCol.gympartner.R
+
+class HomeActivity : AppCompatActivity() {
+
+    companion object {
+        fun create(context: Context) = Intent(context, HomeActivity::class.java)
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
